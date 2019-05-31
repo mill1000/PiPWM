432a7474d6d0244edb235f836c17c021f83b294f
Tucker Kern <tuckkern@gmail.com>
2019-05-30 22:03:34 -0600

    Remove unused PWM register bit defines

--- source/BCM2837/bcm2837_pwm.h
+++ source/BCM2837/bcm2837_pwm.h
@@ -9,42 +9,6 @@
 
 #define PWM_BASE_OFFSET (0x0020C000)
 
-// PWM CTL Register Bits
-#define PWM_CTL_MSEN2 (1 << 15)
-#define PWM_CTL_USEF2 (1 << 13)
-#define PWM_CTL_POLA2 (1 << 12)
-#define PWM_CTL_SBIT2 (1 << 11)
-#define PWM_CTL_RPTL2 (1 << 10)
-#define PWM_CTL_MODE2 (1 << 9)
-#define PWM_CTL_PWEN2 (1 << 8)
-
-#define PWM_CTL_MSEN1 (1 << 7)
-#define PWM_CTL_CLRF1 (1 << 6)
-#define PWM_CTL_USEF1 (1 << 5)
-#define PWM_CTL_POLA1 (1 << 4)
-#define PWM_CTL_SBIT1 (1 << 3)
-#define PWM_CTL_RPTL1 (1 << 2)
-#define PWM_CTL_MODE1 (1 << 1)
-#define PWM_CTL_PWEN1 (1 << 0)
-
-// PWM STA Register Bits
-#define PWM_STA_STA2  (1 << 10)
-#define PWM_STA_STA1  (1 << 9)
-#define PWM_STA_BERR  (1 << 8)
-#define PWM_STA_GAPO2 (1 << 5)
-#define PWM_STA_GAPO1 (1 << 4)
-#define PWM_STA_RERR1 (1 << 3)
-#define PWM_STA_WERR1 (1 << 2)
-#define PWM_STA_EMPT1 (1 << 1)
-#define PWM_STA_FULL1 (1 << 0)
-
-// PWM DMAC Register Bits
-#define PWM_DMAC_ENAB         (1 << 31)
-#define PWM_DMAC_PANIC_SHIFT  (8)
-#define PWM_DMAC_PANIC_MASK   (0xFF << 8)
-#define PWM_DMAC_DREQ_SHIFT   (0)
-#define PWM_DMAC_DREQ_MASK    (0xFF << 0)
-
 typedef struct pwm_control_t
 {
   uint32_t PWEN1 : 1;
