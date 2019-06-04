d0187541abb32e58a6b758eb86defb73ef1d8138
Tucker Kern <tuckkern@gmail.com>
2019-06-04 10:19:25 -0600

    Remove unused DMA defines

--- source/BCM2837/bcm2837_dma.h
+++ source/BCM2837/bcm2837_dma.h
@@ -12,48 +12,6 @@
 #define DMA_INT_STATUS_OFFSET (0xFE0)
 #define DMA_ENABLE_OFFSET     (0xFF0)
 
-// DMA CS Register Control Bits
-#define DMA_CS_CONTROL_MASK                (‭0xFFFFFE00‬)
-#define DMA_CS_RESET                       (1 << 31)
-#define DMA_CS_ABORT                       (1 << 30)
-#define DMA_CS_DISDEBUG                    (1 << 29)
-#define DMA_CS_WAIT_FOR_OUTSTANDING_WRITES (1 << 28)
-#define DMA_CS_PANIC_PRIORITY_SHIFT        (20)
-#define DMA_CS_PANIC_PRIORITY_MASK         (0xF << 20)
-#define DMA_CS_PRIORITY_SHIFT              (16)
-#define DMA_CS_PRIORITY_MASK               (0xF << 16)
-
-// DMA CS Register Status Bits
-#define DMA_CS_STATUS_MASK                    (0x1FF)
-#define DMA_CS_ERROR                          (1 << 8)
-#define DMA_CS_WAITING_FOR_OUTSTANDING_WRITES (1 << 6)
-#define DMA_CS_DREQ_STOPS_DMA                 (1 << 5)
-#define DMA_CS_PAUSED                         (1 << 4)
-#define DMA_CS_DREQ                           (1 << 3)
-#define DMA_CS_INT                            (1 << 2)
-#define DMA_CS_END                            (1 << 1)
-#define DMA_CS_ACTIVE                         (1 << 0)
-
-// DMA TI Register Bits
-#define DMA_TI_NO_WIDE_BURSTS (1 << 26)
-#define DMA_TI_WAITS_SHIFT (21)
-#define DMA_TI_WAITS_MASK (0x1F << 21)
-#define DMA_TI_PERMAP_SHIFT (16)
-#define DMA_TI_PERMAP_MASK (0x1F << 16)
-#define DMA_TI_BURST_LENGTH_SHIFT (12)
-#define DMA_TI_BURST_LENGTH_MASK (0xF << 12)
-#define DMA_TI_SRC_IGNORE (1 << 11)
-#define DMA_TI_SRC_DREQ (1 << 10)
-#define DMA_TI_SRC_WIDTH (1 << 9)
-#define DMA_TI_SRC_INC (1 << 8)
-#define DMA_TI_DEST_IGNORE (1 << 7)
-#define DMA_TI_DEST_DREQ (1 << 6)
-#define DMA_TI_DEST_WIDTH (1 << 5)
-#define DMA_TI_DEST_INC (1 << 4)
-#define DMA_TI_WAIT_RESP (1 << 3)
-#define DMA_TI_TDMODE (1 << 1)
-#define DMA_TI_INTEN (1 << 0)
-
 typedef struct dma_control_status_t
 {
   uint32_t ACTIVE : 1;
