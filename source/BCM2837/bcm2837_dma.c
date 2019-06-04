ada2ca75a53f320a67759f21f0695c08e71e0bdb
Tucker Kern <tuckkern@gmail.com>
2019-06-04 10:21:25 -0600

    Add some DMA access functions

--- source/BCM2837/bcm2837_dma.c
+++ source/BCM2837/bcm2837_dma.c
@@ -32,3 +32,61 @@ static bcm2837_dma_channel_t* dmaGetChannel(dma_channel_t channel)
 
   return (bcm2837_dma_channel_t*) (dma + (channel * DMA_CHANNEL_OFFSET));
 }
+
+/**
+  @brief  Reset the target DMA channel
+
+  @param  channel DMA channel number
+  @retval void
+*/
+void dmaReset(dma_channel_t channel)
+{
+  bcm2837_dma_channel_t* handle = dmaGetChannel(channel);
+
+  WMB();
+  handle->CS.RESET = 1;
+
+  // Clear interupt and end status flags
+  handle->CS.INT = 1;
+  handle->CS.END = 1;
+
+  // Clear error flags in debug register
+  handle->DEBUG.READ_ERROR = 1;
+  handle->DEBUG.FIFO_ERROR = 1;
+  handle->DEBUG.READ_LAST_NOT_SET_ERROR = 1;
+}
+
+/**
+  @brief  Set the control block for the selected DMA channel
+
+  @param  channel DMA channel number
+  @param  control DMA control block to set on channel
+  @retval void
+*/
+void dmaSetControlBlock(dma_channel_t channel, const dma_control_block_t* control)
+{
+  // Ensure block is 256 byte aligned
+  assert(((uint32_t)control & 0x1F) == 0);
+
+  bcm2837_dma_channel_t* handle = dmaGetChannel(channel);
+
+  WMB();
+
+  handle->CONBLK_AD = (dma_control_block_t*) control;
+}
+
+/**
+  @brief  Enable/disable select DMA channel
+
+  @param  channel DMA channel number
+  @param  enable Enable the channel
+  @retval void
+*/
+void dmaEnable(dma_channel_t channel, bool enable)
+{
+  bcm2837_dma_channel_t* handle = dmaGetChannel(channel);
+
+  WMB();
+
+  handle->CS.ACTIVE = enable;
+}
--- source/BCM2837/bcm2837_dma.h
+++ source/BCM2837/bcm2837_dma.h
@@ -245,4 +245,7 @@ typedef enum dma_channel_t
 } dma_channel_t;
 
 void dmaInit(void* base);
+void dmaReset(dma_channel_t channel);
+void dmaSetControlBlock(dma_channel_t channel, const dma_control_block_t* control);
+void dmaEnable(dma_channel_t channel, bool enable);
 #endif
