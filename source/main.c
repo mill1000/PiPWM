a7aebda5e6da5e6c2f63ee041ebd5ed58f43c1c6
Tucker Kern <tuckkern@gmail.com>
2019-06-12 14:27:21 -0600

    Register signal handlers in demo to properly shutdown system

--- source/main.c
+++ source/main.c
@@ -1,14 +1,49 @@
 #include <unistd.h>
 #include <float.h>
 #include <math.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "log.h"
 #include "pipwm.h"
 
 #define TAG "MAIN"
 
+void signalHandler(int signal)
+{
+  LOGW(TAG, "Received signal %s (%d).", sys_siglist[signal], signal);
+
+  piPwm_shutdown();
+
+  exit(EXIT_SUCCESS);
+}
+
+void registerSignalHandler()
+{
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(sa));
+
+  sa.sa_handler = &signalHandler;
+
+  // Register fatal signal handlers
+  sigaction(SIGHUP, &sa, NULL);
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGQUIT, &sa, NULL);
+  sigaction(SIGILL, &sa, NULL);
+  sigaction(SIGABRT, &sa, NULL);
+  sigaction(SIGFPE, &sa, NULL);
+  sigaction(SIGSEGV, &sa, NULL);
+  sigaction(SIGPIPE, &sa, NULL);
+  sigaction(SIGALRM, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
+  sigaction(SIGBUS, &sa, NULL);
+}
+
 int main()
 {
+  registerSignalHandler();
+
   logSetLevel(LOG_LEVEL_DEBUG);
   
   double frequency_Hz = 100;
@@ -66,18 +101,15 @@ int main()
   if (channel == NULL)
     LOGF(TAG, "Failed to initalize PiPWM channel");
 
-  
   piPwm_enableChannel(channel, true);
 
   piPwm_setDutyCycle(channel, .25);
   sleep(2);
 
-  piPwm_setPulseWidth(channel, tStep_s);
-  sleep(2);
+  piPwm_setPulseWidth(channel, 1.52e-3);
 
-  piPwm_releaseChannel(channel);
-
-  piPwm_shutdown();
-
+  while(1)
+    sleep(1);
+    
   return 0;
 }
