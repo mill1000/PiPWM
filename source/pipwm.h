cc2dad37518c41f168dea40842d7089aaff8acb7
Tucker Kern <tuckkern@gmail.com>
2019-06-13 21:32:27 -0600

    Variation that uses 1 DMA channel for dedicated timebase generation so a 2nd DMA channel can output separate pulse widths on pins without requiring 2 * N control blocks per channel

--- source/pipwm.h
+++ source/pipwm.h
@@ -10,9 +10,9 @@
 
 typedef struct pipwm_control_t
 {
-  dma_control_block_t controlBlocks[4];
+  dma_control_block_t controlBlocks[2];
   gpio_pin_mask_t     setMask;
-  gpio_pin_mask_t     clearMask;
+  gpio_pin_mask_t     clearMask[1]; // Actually N - 1, where N is channel resolution
 } pipwm_control_t;
 
 typedef struct pipwm_channel_t
@@ -24,11 +24,11 @@ typedef struct pipwm_channel_t
   uint32_t          steps;
 } pipwm_channel_t;
 
-void piPwm_initialize(uint16_t divi, uint16_t divf, uint16_t range);
+void piPwm_initialize(dma_channel_t dmaChannel, uint16_t divi, uint16_t divf, uint16_t range);
 void piPwm_shutdown(void);
 pipwm_channel_t* piPwm_initalizeChannel(dma_channel_t dmaChannel, gpio_pin_mask_t pinMask, double frequency_Hz);
 void piPwm_releaseChannel(pipwm_channel_t* channel);
 void piPwm_enableChannel(const pipwm_channel_t* channel, bool enable);
-void piPwm_setDutyCycle(pipwm_channel_t* channel, double dutyCycle);
-void piPwm_setPulseWidth(pipwm_channel_t* channel, double pulseWidth_s);
+void piPwm_setDutyCycle(pipwm_channel_t* channel, gpio_pin_mask_t mask, double dutyCycle);
+void piPwm_setPulseWidth(pipwm_channel_t* channel, gpio_pin_mask_t mask, double pulseWidth_s);
 #endif
